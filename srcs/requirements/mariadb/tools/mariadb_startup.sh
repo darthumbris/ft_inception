6869b9f7b1aa3df53546e7c24c59121a43ee72b5
Siebe Hoogenboom <shoogenb@student.codam.nl>
2022-10-04 14:08:34 +0200

    added Dockerfile for the wordpress container

--- srcs/requirements/mariadb/tools/mariadb_startup.sh
+++ srcs/requirements/mariadb/tools/mariadb_startup.sh
@@ -1,15 +1,16 @@
 
 #putting the user and password from the .env into the config
-sed -i -e "s/\${DB_USER}/${DB_USER}/g" ./conf/mariadb.conf;
-sed -i -e "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" ./conf/mariadb.conf;
+sed -i -e "s/\${DB_USER}/${MARIADB_USER}/g" ./conf/mariadb.conf;
+sed -i -e "s/\${DB_PASSWORD}/${MARIADB_PASSWORD}/g" ./conf/mariadb.conf;
 
 #starting up the mysql service
 service mysql start;
 
-echo "Creating user: '${DB_USER}'@'%' with password: '${DB_PASSWORD}';";
-echo "Creating database: ${DB_NAME};";
-echo "Grant priviliges on *.* to: '${DB_USER}'@'%' with grant option;";
-echo "Flush priviliges;";
+#creating user and naming database and grantinc access to user
+echo "CREATING USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';";
+echo "CREATING DATABASE ${MARIADB_NAME};";
+echo "GRANT ALL ON *.* TO '${MARIADB_USER}'@'%' WITH GRANT OPTION;";
+echo "FLUSH PRIVILEGES;";
 
 #stopping the mysql service
 service mysql stop;
